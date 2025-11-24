8ea4fbc1ad474692f927e60b0936a98bcc74b954
Chirag Mehta <chirag.mehta25072005@gmail.com>
2025-11-24 10:53:42 +0530

    fix: force-dynamic added to globe API

--- src/app/api/admin/globe/route.js
+++ src/app/api/admin/globe/route.js
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+export const runtime = "nodejs";  // optional but recommended
 import { NextResponse } from 'next/server';
 import dbConnect from '@/lib/db';
 import GlobeFeed from '@/models/GlobeFeed';
